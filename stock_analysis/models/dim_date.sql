a19aaa6e582e8a20330f3bc565e2072c80c6daab
joshlai <lifestylewiki@gmail.com>
2025-06-14 23:48:06 +0800

    add dim_date.sql and dim_info.sql for dbt run

---
+++ stock_analysis/models/dim_date.sql
@@ -0,0 +1,14 @@
+{{ 
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+  DATE(date) AS date,
+  EXTRACT(YEAR FROM date) AS year,
+  EXTRACT(MONTH FROM date) AS month,
+  EXTRACT(DAY FROM date) AS day,
+  EXTRACT(QUARTER FROM date) AS quarter,
+  FORMAT_TIMESTAMP('%A', date) AS day_of_week
+FROM {{source('stock_analysis', 'combined_tickers')}}
