353eb6e0039a62cea9a3639015e8c2eda58613cf
jacob-data-engineer <jacob.lee.b.g@gmail.com>
2025-06-17 20:38:30 +0800

    add the singapore time zone

--- ingestion_pipe/ingestion_pipe/schedules.py
+++ ingestion_pipe/ingestion_pipe/schedules.py
@@ -5,16 +5,22 @@ from .dagster_wrapper import upload_yfinance_job,dbt_job,dbt_test
 daily_schedule = ScheduleDefinition(
     job=upload_yfinance_job,
     cron_schedule="30 7 * * *",  # ⏰ Cron format: minute hour day month weekday
-    name="daily_upload_yfinance_job"
+    name="daily_upload_yfinance_job",
+    execution_timezone="Asia/Singapore"
+
 )
 daily_dbt_schedule = ScheduleDefinition(
     job=dbt_job,
     cron_schedule="45 7 * * *",
-    name="daily_dbt_job"
+    name="daily_dbt_job",
+    execution_timezone="Asia/Singapore"
+
 )
 
 daily_dbt_test_schedule = ScheduleDefinition(
     job=dbt_test,
     cron_schedule="0 8 * * *",
-    name="daily_dbt_test"
+    name="daily_dbt_test",
+    execution_timezone="Asia/Singapore"
+
 )
