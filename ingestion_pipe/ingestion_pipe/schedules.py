e5c3a511ac2426cc887c9b18b0ca6d3e7b1579b2
jacob-data-engineer <jacob.lee.b.g@gmail.com>
2025-06-17 20:32:36 +0800

    updated schedule

--- ingestion_pipe/ingestion_pipe/schedules.py
+++ ingestion_pipe/ingestion_pipe/schedules.py
@@ -4,17 +4,17 @@ from .dagster_wrapper import upload_yfinance_job,dbt_job,dbt_test
 # Run every day at 8 AM
 daily_schedule = ScheduleDefinition(
     job=upload_yfinance_job,
-    cron_schedule="30 16 * * *",  # ⏰ Cron format: minute hour day month weekday
+    cron_schedule="30 7 * * *",  # ⏰ Cron format: minute hour day month weekday
     name="daily_upload_yfinance_job"
 )
 daily_dbt_schedule = ScheduleDefinition(
     job=dbt_job,
-    cron_schedule="45 16 * * *",
+    cron_schedule="45 7 * * *",
     name="daily_dbt_job"
 )
 
 daily_dbt_test_schedule = ScheduleDefinition(
     job=dbt_test,
-    cron_schedule="45 16 * * *",
+    cron_schedule="0 8 * * *",
     name="daily_dbt_test"
 )
