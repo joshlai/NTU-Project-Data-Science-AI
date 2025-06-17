0ba54600982bb5cdc72a2275344906b61e620e3e
jacob-data-engineer <jacob.lee.b.g@gmail.com>
2025-06-17 15:05:48 +0800

    renamed failes to correct name

--- ingestion_pipe/ingestion_pipe/dagster_wrapper.py
+++ ingestion_pipe/ingestion_pipe/dagster_wrapper.py
@@ -3,43 +3,59 @@ import subprocess
 
 @op
 def run_yfinance_script():
-    subprocess.run(["python", "injestion_yfinance.py"], check=True)
+    try:
+        subprocess.run(
+            ["python", "/home/biscuit/NTU-Project-Data-Science-AI/ingestion_pipe/ingestion_yfinance.py"],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error running ingestion_yfinance.py: {e}")
+        raise
 
 @op
 def run_stock_info_script():
-    subprocess.run(["python", "injestion_stock_info.py"], check=True)
+    try:
+        subprocess.run(
+            ["python", "/home/biscuit/NTU-Project-Data-Science-AI/ingestion_pipe/ingestion_stock_info.py"],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error running injestion_stock_info.py: {e}")
+        raise
 
 @op
 def run_fred_script():
-    subprocess.run(["python", "injestion_fred.py"], check=True)
-
-
-##@op
-##def run_dbt():
-##    subprocess.run(["dbt", "run"], cwd="/home/biscuit/NTU-Project-Data-Science-AI/dbt_yfinance", check=True)
+    try:
+        subprocess.run(
+            ["python", "/home/biscuit/NTU-Project-Data-Science-AI/ingestion_pipe/ingestion_fred.py"],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error running ingestion_fred.py: {e}")
+        raise
 
 @op
 def run_dbt_yfinance():
-    subprocess.run(
-        [
-            "dbt", 
-            "run", 
-            "--project-dir", "/home/biscuit/NTU-Project-Data-Science-AI/dbt_yfinance",
-            "--profiles-dir", "/home/biscuit/NTU-Project-Data-Science-AI/dbt_yfinance"
-        ],
-        check=True
-    )
-
-
-
+    try:
+        subprocess.run(
+            [
+                "dbt",
+                "run",
+                "--project-dir", "/home/biscuit/NTU-Project-Data-Science-AI/dbt_yfinance",
+                "--profiles-dir", "/home/biscuit/NTU-Project-Data-Science-AI/dbt_yfinance",
+            ],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error running dbt: {e}")
+        raise
 
 @job
 def upload_yfinance_job():
     run_yfinance_script()
     run_stock_info_script()
     run_fred_script()
-    
 
 @job
 def dbt_job():
-    run_dbt()
+    run_dbt_yfinance()
