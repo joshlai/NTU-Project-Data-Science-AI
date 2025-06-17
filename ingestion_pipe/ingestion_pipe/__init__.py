b304e3db5e9b110a71eac5ca5044fe9bbaafcf6e
jacob-data-engineer <jacob.lee.b.g@gmail.com>
2025-06-17 15:44:31 +0800

    added file

--- ingestion_pipe/ingestion_pipe/__init__.py
+++ ingestion_pipe/ingestion_pipe/__init__.py
@@ -1,6 +1,6 @@
 from dagster import Definitions
-from .dagster_wrapper import upload_yfinance_job
+from .dagster_wrapper import upload_yfinance_job,dbt_job
 
 defs = Definitions(
-    jobs=[upload_yfinance_job]
+    jobs=[upload_yfinance_job,dbt_job]
 )
